3bf7baf7430267d9a47e22d36de1f86b89e50f03
alegomesilva <89050647+alegomesilva@users.noreply.github.com>
2022-04-21 18:46:07 -0300

    cadastro

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port =  Number(process.env.SERVER_PORT);
 
-app.use('/', express.static('login'));
+app.use('/', express.static('.'));
 
 
 
