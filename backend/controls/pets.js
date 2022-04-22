c962d2f4e1d48f80d26969ef7f9f9963da7c08e0
Emmanuelle Oliveira <emmanuelleamaral@hotmail.com>
2022-04-22 03:32:24 -0300

    Comunicação da página de login com o back-end funcionando e alterações nos middlewares da pasta controls

---
+++ backend/controls/pets.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const {getClient} = require('../utils/client_pg');
+const authorization = require('../middlewares/authorization_users');
+const router = express.Router();
+//http://localhost:3333
+router.get('/', authorization, async (req, res) => {
+    const client = await getClient();
+    const users = await client.query('SELECT * FROM public.pets');
+    await client.end();
+    res.json(users.rows); 
+});
+
+router.post('/', authorization, async (req, res) => {
+    const client = await getClient();
+    const verifyPet = await client.query('SELECT pets.name FROM public.pets WHERE name = $1', [req.body.name]);
+    if(verifyPet.rows.length === 0){
+        const users = await client.query('INSERT INTO public.pets (name, weight, price, height, created_by, created_at, category_id, url_image) VALUES ($1, $2, $3, $4, 1, CURRENT_TIMESTAMP, $5, $6) RETURNING*', [req.body.name, req.body.weight, req.body.price, req.body.height, req.body.category_id, req.body.url_image]);
+        await client.end();
+        res.json(users.rows[0]);
+    } else {
+        res.send("O pet já está cadastrado no banco de dados");
+    }
+});
+
+router.put('/:id', authorization, async (req, res) => {
+    const client = await getClient(); 
+    const verifyDeleted = await client.query('SELECT pets.delete_at FROM public.pets WHERE id = $1', [Number(req.params.id)]);
+    if(verifyDeleted.rows[0]["delete_at"] === null){
+        const users = await client.query('UPDATE public.pets SET name = $1, weight = $2, price=$3, height=$4, update_at = CURRENT_TIMESTAMP, category_id=$5, url_image=$6 WHERE id = $7) RETURNING*', [req.body.name, req.body.weight, req.body.price, req.body.height, req.body.category_id, req.body.url_image, Number(req.params.id)]);
+        await client.end();
+        res.json(users.rows[0]);
+    } else {
+        res.send("O pet foi deletado do banco de dados");
+    }
+});
+
+router.delete('/:id', authorization, async (req, res) => {
+    const client = await getClient(); 
+    const verifyPet = await client.query('SELECT pets.name FROM public.pets WHERE name = $1', [req.body.name]);
+    if(verifyPet.rows.length === 0){
+        const users = await client.query('UPDATE public.pets SET delete_at = CURRENT_TIMESTAMP WHERE id = $1) RETURNING*', [Number(req.params.id)]);
+        await client.end();
+        res.json(users.rows[0]);
+    } else {
+        res.send("O pet não está cadastrado no banco de dados");
+    }
+});
+
+module.exports = router;
---
+++ backend/controls/users.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const {getClient} = require('../utils/client_pg');
+const authorization = require('../middlewares/authorization_users.js');
+const router = express.Router();
+//http://localhost:3333
+router.get('/', authorization, async (req, res) => {
+    const client = await getClient();
+    const users = await client.query('SELECT users.username FROM public.users');
+    await client.end();
+    res.json(users.rows); 
+});
+
+router.post('/', authorization, async (req, res) => {
+    const client = await getClient(); 
+    const verifyUsername = await client.query('SELECT users.username FROM public.users WHERE username = $1', [req.body.username]);
+    if(verifyUsername.rows.length === 0) {
+        const users = await client.query('INSERT INTO public.users (username, password, created_at, created_by) VALUES ($1, $2, CURRENT_TIMESTAMP, 1) RETURNING*', [req.body.username, req.body.password]);
+        await client.end();
+        res.json(users.rows[0]);
+    } else {
+        res.send("O username já está sendo utilizado por outro usuário");
+    }
+});
+
+router.put('/:id', authorization, async (req, res) => {
+    const client = await getClient(); 
+    const verifyDeleted = await client.query('SELECT clients.delete_at FROM public.clients WHERE id = $1', [Number(req.params.id)]);
+    if(verifyDeleted.rows[0]["delete_at"] === null) { 
+        const users = await client.query('UPDATE public.users SET username = $1, password = $2, update_at = CURRENT_TIMESTAMP WHERE id = $3) RETURNING*', [req.body.username, req.body.password, Number(req.params.id)]);
+        await client.end();
+        res.json(users.rows[0]);
+    } else {
+        res.send("Usuário deletado do banco de dados");
+    }
+});
+
+router.delete('/:id', authorization, async (req, res) => {
+    const client = await getClient(); 
+    const verifyUser = await client.query('SELECT users.username FROM public.users WHERE username = $1', [req.body.username]);
+    if(verifyUser.rows.length === 0){
+        const users = await client.query('UPDATE public.users SET delete_at = CURRENT_TIMESTAMP WHERE id = $1) RETURNING*', [Number(req.params.id)]);
+        await client.end();
+        res.json(users.rows[0]);
+    } else {
+        res.send("O usuário não está cadastrado no banco de dados");
+    }
+});
+
+module.exports = router;
