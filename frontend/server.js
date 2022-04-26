4b282a7b08aef2eda55a9e96c8bdb85bc6b52109
alegomesilva <89050647+alegomesilva@users.noreply.github.com>
2022-04-26 00:36:28 -0300

    pagina detalhe

--- frontend/server.js
+++ frontend/server.js
@@ -6,7 +6,7 @@ const port =  Number(process.env.SERVER_PORT);
 app.use('/login', express.static('login'));
 app.use('/cadastro', express.static('cadastro'));
 app.use('/', express.static('assets'));
-
 app.use('/inicial', express.static('inicial'));
+app.use('/detalhe', express.static('detalhe'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
